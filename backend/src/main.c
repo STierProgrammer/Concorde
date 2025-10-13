e7df5425aa93fd016c842becbba284d12b3e2f84
STierProgrammer <sabasajaia40@gmail.com>
2025-10-13 20:31:23 +0400

    [root/meson.build] Added sqlite3 as a dependency

--- backend/src/main.c
+++ backend/src/main.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
+#include <sqlite3.h>
 
-int main() {
-    printf("Hello, World!");
+int main()
+{
+    sqlite3 *db;
+    int rc;
+
+    rc = sqlite3_open("test.db", &db);
+
+    if (rc)
+    {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return (0);
+    }
+    else
+    {
+        fprintf(stderr, "Opened database successfully\n");
+    }
+
+    sqlite3_close(db);
 }
